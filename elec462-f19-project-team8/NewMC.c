7d698421e6994acc8674683ec20380750cf98a9e
nam7855 <hyun97727855@gmail.com>
2019-12-13 23:21:39 +0900

    update PageHandling And Exit
    "file1~4" and "folder 1~16" are
    testing files and folders for paging
    
    +stretched code as much as possible...
      but still 432 lines...

--- elec462-f19-project-team8/NewMC.c
+++ elec462-f19-project-team8/NewMC.c
@@ -24,7 +24,9 @@ struct item{
 char pwd[BUFSIZ - 10];
 char buf[BUFSIZ];
 int count;
+int page;
 int cur_row;
+int cur_page;
 struct item data[BUFSIZ];
 
 void print_pwd();
@@ -43,6 +45,7 @@ void _rm();
 
 int main(int argc, char* argv[])
 {
+	signal(SIGINT, SIG_IGN); signal(SIGQUIT, SIG_IGN); signal(SIGTSTP, SIG_IGN);
 	initscr();
 
 	crmode();
@@ -53,11 +56,11 @@ int main(int argc, char* argv[])
 
 	start_color();
 	
-	init_color(COLOR_BLACK, 200, 200, 500);
+	init_color(COLOR_BLACK, 180, 200, 300);
 	init_color(COLOR_YELLOW, 1000, 1000, 0);
 	init_pair(SELECT, COLOR_YELLOW, COLOR_BLACK);
 
-	int c; cur_row = 0;
+	int c; cur_row = 0; cur_page = 0;
 	FILE* fp; struct stat sbuf;
 
 	fp = popen("pwd", "r");
@@ -73,24 +76,59 @@ int main(int argc, char* argv[])
 
 		switch(c = getch())
 		{
-			case KEY_DOWN: if(cur_row+1<count)cur_row++; break;
-			case KEY_UP: if(cur_row>0)cur_row--; break;
-			case '\n': 
-					     if(isadir(ch_fname(data[cur_row].name))){
-						     //if directory. chdir.
-						     chdir(ch_fname(data[cur_row].name));
-						     getcwd(pwd, BUFSIZ-10);
-						     load_ls();
-						     cur_row = 0;
-					     }
-					     break;
-			case KEY_F(5):_cp(); load_ls(); print_ls(); break;//file copy
-			case KEY_F(6):_mv(); load_ls(); print_ls(); break;//file mv
-			case KEY_F(7):_mkdir(); load_ls(); print_ls(); break;//mkdir
-			case KEY_F(8):_rm(); load_ls(); print_ls(); break;//file delete
-			case KEY_HOME: cur_row = 0; break;
-			case KEY_END:  cur_row = count - 1; break;
-			case KEY_IC: if(cur_row != 0)data[cur_row].check *= -1; if(cur_row+1<count)cur_row++; break;
+			case KEY_DOWN: if(cur_page < page){
+						if(cur_row+1<15) cur_row++; 
+						else {cur_row=0; cur_page++;}
+					}
+					else if(cur_page == page && cur_page*15 + cur_row+1<count) cur_row++;
+					break;
+
+			case KEY_UP: if(cur_row>0)cur_row--; else if(cur_page > 0){cur_row=14; cur_page--;} break;
+
+			case '\n': if(isadir(ch_fname(data[cur_page*15 + cur_row].name))){
+					//if directory. chdir.
+					chdir(ch_fname(data[cur_page*15 + cur_row].name));
+					getcwd(pwd, BUFSIZ-10);
+					load_ls();
+					cur_row = 0; cur_page = 0;
+					}
+					break;
+
+			case KEY_F(5):	_cp();
+					load_ls();
+					print_ls();
+					break;		//file copy
+
+			case KEY_F(6):	_mv();
+					load_ls();
+					print_ls();
+					break;		//file mv
+
+			case KEY_F(7):	_mkdir();
+					load_ls();
+					print_ls();
+					break;		//mkdir
+
+			case KEY_F(8):	_rm();
+					load_ls();
+					print_ls();
+					break;		//file delete
+
+			case KEY_F(9):	pclose(fp);
+					endwin();
+					return 1;	//quit
+
+			case KEY_HOME:	cur_row = 0; 
+					break;		//cur move top
+
+			case KEY_END:	cur_row = count - 1;
+					break;		//cur move bottom
+
+			case KEY_IC:	if(cur_page*15 + cur_row != 0)
+						data[cur_page*15 + cur_row].check *= -1;
+					if((cur_page < page && cur_row+1<15) || (cur_page == page && cur_page*15 + cur_row +1<count))
+						cur_row++;
+					break;
 			default: break;
 		}
 	}
@@ -102,6 +140,8 @@ int main(int argc, char* argv[])
 void print_pwd()
 {	
 	clear();
+	move(PWD-1, 5);
+	printw("[[ Current Directory ]]");
 	move(PWD, 5);
 	standout();
 	addstr(pwd);
@@ -117,7 +157,6 @@ void load_ls()
 	strcat(command, pwd);
 
 	count = 0;
-
 	fp = popen(command, "r");
 	fgets(buf, BUFSIZ, fp);	// info
 	fgets(buf, BUFSIZ, fp);	// ./
@@ -127,26 +166,52 @@ void load_ls()
 		strcpy(data[count].name, buf);
 		count++;
 	}
+	page = count/15;
 	pclose(fp);
 }
 
 void print_ls()
 {
-	move(LINES-2, 0);
-	printw("%d Files in this directory", count - 1);
-	for(int i = 0; i < count; i++)
+	move(LINES-3, 0);
+	attron(A_UNDERLINE);
+	printw("%d Files in this directory               Page (%d / %d)", count - 1, cur_page+1, page+1);
+	attroff(A_UNDERLINE);
+	move(LS-1, 5);
+	printw("[[ File List ]]");
+
+	if(cur_page < page)
 	{
-		move(LS + i, 5);
-		if(i == cur_row)
+		for(int i = cur_page*15; i < (cur_page+1)*15; i++)
 		{
-			move(LS + i, 2);
-			printw(">> ");
+			move(LS + i%15, 5);
+			if(i == (cur_row + cur_page*15))
+			{
+				move(LS + i%15, 2);
+				printw(">> ");
+			}
+			if(data[i].check == -1)
+				attron(COLOR_PAIR(SELECT));
+			printw("%s", data[i].name);
+			if(data[i].check == -1)
+				attroff(COLOR_PAIR(SELECT));
 		}
-		if(data[i].check == -1)
-			attron(COLOR_PAIR(SELECT));
-		printw("%s", data[i].name);
-		if(data[i].check == -1)
-			attroff(COLOR_PAIR(SELECT));
+	}
+	else
+	{
+		for(int i = page*15; i < count; i++)
+		{
+			move(LS + i%15, 5);
+			if(i == (cur_row + page*15))
+			{
+				move(LS + i%15, 2);
+				printw(">> ");
+			}
+			if(data[i].check == -1)
+				attron(COLOR_PAIR(SELECT));
+			printw("%s", data[i].name);
+			if(data[i].check == -1)
+				attroff(COLOR_PAIR(SELECT));
+		}
 	}
 	move(LINES-1, COLS-1);//just in case
 	curs_set(0);
@@ -155,14 +220,18 @@ void print_ls()
 
 void print_menu()
 {
-	char* menu[10] = {"Help", "Menu", "View", "Edit", "Copy", "RenMov", "Mkdir", "Delete", "PullDn", "Quit"};
-	move(LINES-1, 0);
-	for(int i = 0; i < 10; i++)
-		printw("%-2d%-8s", i + 1, menu[i]);
+	char* menu[10] = {"Help", "Menu", "View", "Edit", "Copy", "Ren/Mov", "Mkdir", "Delete", "Quit"};
+	move(LINES-2, 0);
+	for(int i = 0; i < 9; i++)
+	{
+		printw("F%-2d %-8s", i + 1, menu[i]);
+		if(i==4) move(LINES-1, 0);
+	}
 	move(LINES-1, COLS-1);
 }
 
-char* ch_fname(char* f){
+char* ch_fname(char* f)
+{
 	char fname[30], *name, *filename;
 	//filename extraction
 	strcpy(fname, "./");
@@ -176,30 +245,36 @@ char* ch_fname(char* f){
 	return filename;
 }
 
-int isadir(char* f){
+int isadir(char* f)
+{
 	struct stat info;
 	return (stat(f,&info)!=-1 && S_ISDIR(info.st_mode));
 }
 
-void input_set(int b){//1 is set, 0 is unset
-	if(b){
-		move(count+7, 3);
+void input_set(int b)//1 is set, 0 is unset
+{
+	if(b)
+	{
+		move(LINES-7, 0);
 		clrtobot();//wipe
 		curs_set(b); 
 		echo(); 
 		refresh();
 	}
-	else{
+	else
+	{
 		curs_set(b);
 		noecho();
 	}
 }
 
-void child_waiter(int signum){
+void child_waiter(int signum)
+{
 	while(waitpid(-1, NULL, WNOHANG)>0);
 }
 
-void _cp(){
+void _cp()
+{
 	char sourcefile[MAX][BUFSIZ], targetfile[MAX][BUFSIZ], buf[BUFSIZ];
 	int pid, num=0;
 	signal(SIGCHLD, child_waiter);
@@ -209,79 +284,101 @@ void _cp(){
 		if(data[i].check == -1){
 			strcpy(sourcefile[num], ch_fname(data[i].name));
 			input_set(1);
-			printw("input filename : cp %s ", sourcefile[num]);
+			printw("('quit' to return)   input filename : cp %s : ", sourcefile[num]);
 			scanw("%s", buf);
+			if(strcmp(buf, "quit")==0) return;
 			strcpy(targetfile[num++], buf);
 		}
 	}
 	input_set(0);
 
 	//exec cp source target (num is checking num)
-	for(int i=0; i<num; i++){
+	for(int i=0; i<num; i++)
+	{
 		if((pid=fork())==-1) return;
-		if(pid ==0){
+		if(pid ==0)
+		{
 			execlp("cp", "cp", sourcefile[i], targetfile[i], NULL);
 			exit(1);
 		}
 	}
 }
 
-void _mv(){
+void _mv()
+{
 	char buf[BUFSIZ];
 	int pid, num=0;
 	int mv_stat = 0;//0 : mv file1 file2     1: mv file1 dir1
 	signal(SIGCHLD, child_waiter);
 
 	input_set(1);
-	printw("selecet mode : 1. rename  2. move directory >> ");
-	do{
+	printw("selecet mode : 1. rename  2. move directory  3. return >> ");
+	do
+	{
 		scanw("%d ", &mv_stat);
+		if(mv_stat == 3) return;
 		if(mv_stat !=1 && mv_stat != 2)
-			printw("please input 1 or 2");
+			printw("please input 1 or 2 or 3 >> ");
 		else break;
-	}while(1);
+	}
+	while(1);
 
-	if(mv_stat == 1){//rename files selected
+	if(mv_stat == 1)	//rename files selected
+	{
 		char sourcefile[MAX][BUFSIZ], targetfile[MAX][BUFSIZ];
+
 		//input targetfile names
-		for(int i =0; i<count; i++){
-			if(data[i].check == -1){
+		for(int i =0; i<count; i++)
+		{
+			if(data[i].check == -1)
+			{
 				strcpy(sourcefile[num], ch_fname(data[i].name));
 				input_set(1);
-				printw("input filename : mv %s ", sourcefile[num]);
+				printw("('quit' to return)   input filename : mv %s : ", sourcefile[num]);
 				scanw("%s", buf);
+				if(strcmp(buf, "quit")==0) return;
 				strcpy(targetfile[num++], buf);
 			}
 		}
+
 		input_set(0);	
 
 		//exec cp source target (num is checking num)
-		for(int i=0; i<num; i++){
+		for(int i=0; i<num; i++)
+		{
 			if((pid=fork())==-1) return;
-			if(pid ==0){
+			if(pid ==0)
+			{
 				execlp("mv", "mv", sourcefile[i], targetfile[i], NULL);
 				exit(1);
 			}
 		}
 	}
-	else{//move on another directory
+	else	//move on another directory
+	{
 		char *arglist[MAX];
 
 		input_set(1);
-		addstr("input dirname: ");
+		addstr("('quit' to return)   input dirname: ");
 		scanw("%s", buf);
+		if(strcmp(buf, "quit")==0) return;
 
 		arglist[num++] = "mv";
-		for(int i=0; i<count; i++){//mv file1 file2 file3... dir1
+
+		//mv file1 file2 file3... dir1
+		for(int i=0; i<count; i++)
+		{
 			if(data[i].check == -1)
 				arglist[num++] = ch_fname(data[i].name);
 		}
+	
 		arglist[num++] = buf;
 		arglist[num] = 0;
 		input_set(0);
 
 		if((pid=fork())==-1) return;
-		if(pid ==0){
+		if(pid ==0)
+		{
 			execvp(arglist[0], arglist);
 			exit(1);
 		}
@@ -294,16 +391,18 @@ void _mkdir(){
 	signal(SIGCHLD, child_waiter);
 
 	input_set(1);
-	addstr("input dirname: ");
+	addstr("('quit' to return)   input dirname : ");
 
 	arglist[0] = "mkdir";
 	scanw("%s", buf);
+	if(strcmp(buf, "quit") == 0) return;
 	arglist[len++] = buf;
 	arglist[len] = 0;
 	input_set(0);
 
 	if((pid=fork())==-1) return;
-	if(pid ==0){
+	if(pid ==0)
+	{
 		execvp(arglist[0], arglist);
 		exit(1);
 	}
@@ -314,14 +413,19 @@ void _rm(){
 	arglist[0] = "rm";
 	int pid, len=1;
 	signal(SIGCHLD, child_waiter);
-	for(int i=0; i<count; i++){//rm file1 file2 file3...
+
+	//rm file1 file2 file3...
+	for(int i=0; i<count; i++)
+	{
 		if(data[i].check == -1)
 			arglist[len++] = ch_fname(data[i].name);
 	}
+
 	arglist[len] = 0;
 
 	if((pid=fork())==-1) return;
-	if(pid ==0){
+	if(pid ==0)
+	{
 		execvp(arglist[0], arglist);
 		exit(1);
 	}
